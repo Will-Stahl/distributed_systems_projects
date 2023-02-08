c920f6ba97986c17d0882c193c06ec9707a026fa
cloutburst <jashwinacharyapersonal@gmail.com>
2023-02-07 20:26:34 -0600

    Added formatting checks for command line prompts and renamed a few functions

--- src/PubSubClient.java
+++ src/PubSubClient.java
@@ -9,31 +9,45 @@ import java.util.*;
 public class PubSubClient {
     final static int PORT_NUMBER = 1099;
 
-    private static String getAndValidateClientRequest(){
-        Set<String> validRequestType = new HashSet<String>();
-        validRequestType.add("join");
-        validRequestType.add("leave");
-        validRequestType.add("publish");
-        validRequestType.add("subscribe");
-        validRequestType.add("unsubscribe");
-        validRequestType.add("ping");
+    private static void PrintWelcomeMessage(){
+        System.out.println("Before entering your request, please see the following rules for the 6 operations that can be performed (none of these are case sensitive): ");
+        System.out.println("Command for joining a server: Join");
+        System.out.println("Command for leaving a server: Leave");
+        System.out.println("Command for publishing to a server: \"Publish: <Article Name>\". For example: \"Publish: Sports;UMN;;contents\" ");
+        System.out.println("Command for subscribing to an article: \"Subscribe: <Article Name>\". For example: \"Subscribe: Sports;UMN;;\"");
+        System.out.println("Command for unsubscribing from an article: \"Unsubscribe: <Article Name>\". For example: \"Unsubscribe: Sports;UMN;;\"");
+    }
 
+    private static String GetAndValidateClientRequest(){
         Scanner sc = new Scanner(System.in);
         String clientRequest = "";
+        PrintWelcomeMessage();
         while (true){
-            System.out.println("Enter your request to the server:");
             clientRequest = sc.nextLine();
-            String[] words = clientRequest.trim().split(" ");
-            if (validRequestType.contains(words[0].toLowerCase())) break;
-            System.out.println("Error in request! Requests must start with one of the following operations (not case sensitive): Join, Leave, Publish, Subscribe, Unsubscribe, Ping");
+            clientRequest = clientRequest.trim().toLowerCase();
+
+            // If we have a join, leave or ping message, then we can simply break from the loop
+            if (clientRequest.startsWith("join") || clientRequest.startsWith("leave") || clientRequest.startsWith("ping")) break;
+
+            // If the publish, subscribe or unsubscribe command format is valid, then break; otherwise, ask for a correctly formatted input from the client(s).
+            if ((ValidPublishSubOrUnSubCommandFormat(clientRequest))) break;
+            else if (clientRequest.startsWith("publish:")) System.out.println("Invalid Publish Command format. Please use \"Publish: <Article Name>\"");
+            else if (clientRequest.startsWith("subscribe:")) System.out.println("Invalid Subscribe Command format. Please use \"Subscribe: <Article Name>\"");
+            else if (clientRequest.startsWith("unsubscribe:")) System.out.println("Invalid Unsubscribe Command format. Please use \"Unsubscribe: <Article Name>\"");
+            else System.out.println("Error in request! Requests must start with one of the following operations (not case sensitive): Join, Leave, Publish, Subscribe, Unsubscribe, Ping");
         }
         sc.close();
         return clientRequest.trim();
     }
 
-    private static void sendClientRequestToServer(DatagramSocket socket, InetAddress address){
+    private static boolean ValidPublishSubOrUnSubCommandFormat(String clientRequest){
+        String [] words = clientRequest.split(":");
+        return (words.length == 2) && (words[0] == "publish:" || words[0] == "subscribe" || words[0] == "unsubscribe");
+    }
+
+    private static void SendClientRequestToServer(DatagramSocket socket, InetAddress address){
         try{
-            String clientRequest = getAndValidateClientRequest();
+            String clientRequest = GetAndValidateClientRequest();
             byte[] requestBuffer = clientRequest.getBytes();
             DatagramPacket packet = new DatagramPacket(requestBuffer, requestBuffer.length, address, PORT_NUMBER);
             socket.send(packet);
@@ -42,7 +56,7 @@ public class PubSubClient {
         }
     }
 
-    private static void processClientResponseFromServer(DatagramSocket socket, InetAddress address){
+    private static void ProcessClientResponseFromServer(DatagramSocket socket, InetAddress address){
         try{
             byte[] serverResponse = new byte[1024];
             DatagramPacket packet = new DatagramPacket(serverResponse, serverResponse.length);
@@ -67,7 +81,7 @@ public class PubSubClient {
                 @Override
                 public void run(){
                     while (true){
-                        sendClientRequestToServer(socket, address);
+                        SendClientRequestToServer(socket, address);
                     }
                 }
             }).start();
@@ -77,7 +91,7 @@ public class PubSubClient {
                 @Override
                 public void run(){
                     while (true){
-                        processClientResponseFromServer(socket, address);
+                        ProcessClientResponseFromServer(socket, address);
                     }
                 }
             }).start();
