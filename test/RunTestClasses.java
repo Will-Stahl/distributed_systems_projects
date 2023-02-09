0826e424e69a67aa79116f77dfb86e1ae0382413
William Stahl <stahl186@umn.edu>
2023-02-08 18:37:18 -0600

    notes for how to compile and run RunTestClasses

--- test/RunTestClasses.java
+++ test/RunTestClasses.java
@@ -2,6 +2,8 @@ import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 
+// compile with: javac -cp ./../lib/junit-4.13.2.jar:. RunTestClasses.java 
+// run with:     java -cp ./../lib/junit-4.13.2.jar:./../lib/hamcrest-core-1.3.jar:. RunTestClasses 
 public class RunTestClasses {
     public static void main(String[] args) {
         Result result = JUnitCore.runClasses(PubSubPrivateMethodsTest.class);
