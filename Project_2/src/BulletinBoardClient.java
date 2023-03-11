5c8cf5de4f6081b6a79ac3c706926fb0f7e1717f
cloutburst <jashwinacharyapersonal@gmail.com>
2023-03-10 23:02:50 -0600

    Commented out code that was producing errors and added functionality for clients to join any server of their choosing

--- Project_2/src/BulletinBoardClient.java
+++ Project_2/src/BulletinBoardClient.java
@@ -6,38 +6,100 @@ import java.net.InetAddress;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.io.IOException;
+import java.io.Serializable;
 import java.lang.reflect.Array;
 import java.util.*;
 
 public class BulletinBoardClient {
+    private ArrayList<BulletinBoardServer> joinedServers;
+    private ArrayList<String> articles;
+    private int clientPort;
+    private InetAddress IP;
 
-    private static boolean CheckValidPort(int port){
-        Set<Integer> ports = new HashSet<>();
-        ports.add(2000);
-        ports.add(2001);
-        ports.add(2002);
-        ports.add(2003);
-        ports.add(2004);
-
-        return ports.contains(port);
+    public BulletinBoardClient(InetAddress IP) {
+        joinedServers = new ArrayList<>();
+        articles = new ArrayList<>();
+        clientPort = SetRandomClientPortNumber();
+        this.IP = IP;
     }
 
-    public static void main(String[] args){
-        if (args.length != 2){
-            System.out.println("\n[SERVER]: Usage: java BulletinBoardClient <hostname> <server_port>");
-            System.out.println("[CLIENT]: Exiting...");
+    // Function for getting a random port number
+    private static int SetRandomClientPortNumber(){
+        Random rand = new Random();
+        return (rand.nextInt((65535 - 1024) + 1)) + 1024;
+    }
+
+    public String GetAndValidateClientRequest(){
+        Scanner sc = new Scanner(System.in);
+        String clientRequest = "";
+        while (true){
+            System.out.println("\n[CLIENT]: Enter command: ");
+            clientRequest = sc.nextLine();
+            String lowerCaseRequest = clientRequest.trim().toLowerCase();
+
+            // If we have a join or leave essage, then we can simply break from the loop
+            if (lowerCaseRequest.startsWith("join") ||  lowerCaseRequest.startsWith("leave")){
+                break;
+            }
+
+            System.out.println("\nOnly the following 6 operations can be performed by the client:");
+            DisplayOptions();
+        }
+        return clientRequest.trim();
+    }
+
+    /**
+     * Function for displaying menu options
+     **/
+    private static void DisplayOptions(){
+        System.out.println("Ports currently available to join: 2000, 2001, 2002, 2003 and 2004");
+        System.out.println("1. Enter \"Join\" to join the group server.");
+        System.out.println("2. Enter \"Leave\" to leave the group server.");
+    }
+
+    /**
+     * Function for making RMI calls to the group server depending on the client's request.
+     * @param server - Server object for making RMI calls
+     * @param address - Client address
+     */
+    public void SendClientRequestToServer(String hostName, InetAddress address){
+        String IP = address.getHostAddress();
+        String clientRequest = GetAndValidateClientRequest();
+        String[] parts = clientRequest.split(":");
+        String command = parts[0].toLowerCase();
+        int serverPort = Integer.parseInt(parts[1].trim());
+        BulletinBoardServerInterface server = ConnectToServer(hostName, serverPort);
+
+        try{
+            if (command.equals("join")){
+                boolean join = server.Join(IP, clientPort);
+                if (join){
+                    System.out.printf("[CLIENT]: Client at port %d successfully joined server at port %d", clientPort, server.GetServerPort());
+                } else {
+                    System.out.printf("[CLIENT]: It's possible that server capacity has been reached or the IP address provided is invalid.", clientPort, server.GetServerPort());
+                }
+            } else if (command.equals("leave")){
+                boolean leave = server.Leave(IP, clientPort);
+                if (leave){
+                    System.out.printf("[CLIENT]: Client at port %d successfully left server at port %d", clientPort, server.GetServerPort());
+                } else {
+                    System.out.printf("[CLIENT]: Error occurred while leaving server at port %d", clientPort, server.GetServerPort());
+                }
+            }
+        } catch (RemoteException e){
+            System.out.println("[CLIENT]: No response from server since it is offline. Try joining another server!");
+            e.printStackTrace();
             System.exit(0);
         }
+    }
 
-        int serverPort = Integer.parseInt(args[1]);
+    private static BulletinBoardServerInterface ConnectToServer(String hostName, int serverPort){
         // If port is invalid, then print error message and exit.
         if (!CheckValidPort(serverPort)){
             System.out.println("\n[CLIENT]: Port number specified is invalid. Valid port numbers are 2000, 2001, 2002, 2003 and 2004");
             System.out.println("[CLIENT]: Exiting...");
             System.exit(0);
         }
-
-        String hostName = args[0];
 
         // Fixed set of ports are mapped to specific server numbers
         HashMap<Integer, Integer> portToServerMap = new HashMap<>();
@@ -51,12 +113,52 @@ public class BulletinBoardClient {
             int serverNumber = portToServerMap.get(serverPort);
             Registry registry = LocateRegistry.getRegistry(hostName, serverPort);
             BulletinBoardServerInterface server = (BulletinBoardServerInterface) registry.lookup("BulletinBoardServer_" + serverNumber);
-            System.out.println("\n[CLIENT]: Registry lookup was successful!");
+            System.out.println("\n[CLIENT]: Registry lookup was successful! Joining server now...");
+            return server;
         } catch (Exception e){
             System.out.println("\n[CLIENT]: Error occurred while looking up the registry name. Make sure the server is running on the respective port before running the client script.");
             System.out.println("[CLIENT]: Exiting...");
+            e.printStackTrace();
             System.exit(0);
         }
-        
+        return null;
+    }
+
+
+    private static boolean CheckValidPort(int port){
+        Set<Integer> ports = new HashSet<>();
+        ports.add(2000);
+        ports.add(2001);
+        ports.add(2002);
+        ports.add(2003);
+        ports.add(2004);
+
+        return ports.contains(port);
+    }
+
+    public static void main(String[] args){
+        if (args.length != 1){
+            System.out.println("\n[SERVER]: Usage: java BulletinBoardClient <hostname>");
+            System.out.println("[CLIENT]: Exiting...");
+            System.exit(0);
+        }
+
+        String hostName = args[0];
+        try{
+            InetAddress address = InetAddress.getByName(hostName);
+            BulletinBoardClient client = new BulletinBoardClient(address);
+            // Thread for sending client requests to server
+            new Thread(new Runnable(){
+                @Override
+                public void run(){
+                    while(true){
+                        client.SendClientRequestToServer(args[0], address);
+                    }
+                }
+            }).start();
+        } catch (Exception e){
+            System.out.println("[CLIENT]: Error occurred. Exiting...");
+            System.exit(0);
+        }
     }
 }
--- Project_2/src/BulletinBoardServer.java
+++ Project_2/src/BulletinBoardServer.java
@@ -12,24 +12,29 @@ import java.util.*;
 
 // TODO: simulate network delays, probably via wrapper/decorator class
 public class BulletinBoardServer extends UnicastRemoteObject
-implements BulletinBoardServerInterface, ServerToServerInterface {
+implements BulletinBoardServerInterface /*, ServerToServerInterface*/ {
     
-    private int serverPort; 
-    private int serverNum;
+    private Integer serverPort; 
+    private Integer serverNum;
+    private static ArrayList<BulletinBoardClient> clients = new ArrayList<>();
+    private static ArrayList<String> articles = new ArrayList<>();
+    private final int MAX_CLIENTS = 5;
+    private static int clientCount = 0;
 
     // P2P data structures
     private int coordNum;
     private int coordPort;
     private String coordHost;
     private int nextID;
-    private ConsistencyStrategy cStrat;  // initialize to specifc strategy
-    private ReferencedTree contentTree;  // article tree data structure
+    //private ConsistencyStrategy cStrat;  // initialize to specifc strategy
+    //private ReferencedTree contentTree;  // article tree data structure
 
     public BulletinBoardServer(int serverPort, int serverNum,
         String consistency) throws RemoteException{
         this.serverPort = serverPort;
         this.serverNum = serverNum;
         nextID = 1;
+        /* 
         contentTree = new ReferencedTree();
         
         // TODO: intialize article tree, maybe find coordinator, initialize consitency strategy object
@@ -45,10 +50,10 @@ implements BulletinBoardServerInterface, ServerToServerInterface {
         else {
             System.out.println("Invalid strategy entered, defaulting to sequential");
             cStrat = new SequentialStrategy();
-        }
+        }*/
     }
 
-    public int GetServerPort(){
+    public int GetServerPort() throws RemoteException{
         return serverPort;
     }
 
@@ -58,13 +63,22 @@ implements BulletinBoardServerInterface, ServerToServerInterface {
 
     public boolean Join(String IP, int Port) throws RemoteException
     {
+        
         if (Port > 65535 || Port < 0) {
             System.out.println("[SERVER]: Client port number is invalid and cannot be used for communication.");
             return false;
         }
 
+        // If server is at max capacity, then prompt user to try joining again at a later time.
+        if (clientCount == MAX_CLIENTS) {
+
+            System.out.println("[SERVER]: Server Capacity reached! Please try joining again later.");
+            return false;
+        }
+
         // check for valid IP address
         if (IsValidIPAddress(IP)){
+            // TODO: Add clients
             System.out.printf("\n[SERVER]: Added new client with IP: %s, Port: %d\n", IP, Port);
             return true;
         }
@@ -92,7 +106,7 @@ implements BulletinBoardServerInterface, ServerToServerInterface {
      * @param article content to publish
      * if primary server, uses strategy (non-remote method)
      * if not, uses RMI to contact primary
-     */
+     
     public boolean Publish(String article) throws RemoteException {
         int replyTo = 0;  // publishing replies to root
         if (coordNum == serverNum) {  // this server is the coordinator
@@ -100,61 +114,89 @@ implements BulletinBoardServerInterface, ServerToServerInterface {
             return cStrat.ServerPublish(nextID++, article, serverPort,
                                         serverNum, contentTree);
         }
-        // else look up coordinator in registry, request it to publish
-        Registry registry = LocateRegistry.getRegistry(coordHost, coordPort);
-        ServerToServerInterface coordServer = (ServerToServerInterface)
+        try{
+            // else look up coordinator in registry, request it to publish
+            Registry registry = LocateRegistry.getRegistry(coordHost, coordPort);
+            ServerToServerInterface coordServer = (ServerToServerInterface)
             registry.lookup("BulletinBoardServer_" + coordNum);
-        return coordServer.CoordinatorPost(article, replyTo);
+            //return coordServer.CoordinatorPost(article, replyTo);
+            return true;
+        } catch (Exception e){
+            return false;
+        }
     }
+    */
 
     /**
      * @param article content to publish
      * @param replyTo ID of article to reply to
      * if primary server, uses strategy (non-remote method)
      * if not, uses RMI to contact primary
-     */
+     
     public boolean Reply(String article, int replyTo) throws RemoteException {
-        int replyTo = 0;  // publishing replies to root
+        replyTo = 0;  // publishing replies to root
         if (coordNum == serverNum) {  // this server is the coordinator
             // increment nextID after call
             return cStrat.ServerPublish(nextID++, article, serverPort,
                                         serverNum, contentTree);
         }
         // else look up coordinator in registry, request it to publish
-        Registry registry = LocateRegistry.getRegistry(coordHost, coordPort);
-        ServerToServerInterface coordServer = (ServerToServerInterface)
+        try{
+            Registry registry = LocateRegistry.getRegistry(coordHost, coordPort);
+            ServerToServerInterface coordServer = (ServerToServerInterface)
             registry.lookup("BulletinBoardServer_" + coordNum);
-        return coordServer.CoordinatorPost(article, replyTo);
+            //return coordServer.CoordinatorPost(article, replyTo);
+            return true;
+        } catch (Exception e){
+            return false;
+        }
+        
+        
     }
+    */
 
     /**
      * returns indented string previewing all articles with ID
      * for now, we delegate viewing details to client
-     */
-    public String Read() throws RemoteException {
+     
+    public boolean Read() throws RemoteException {
         if (serverNum == coordNum) {  // if this server is the coordinator
-            return cStrat.ServerRead();
+            //return cStrat.ServerRead();
+            return true;
         }
-        // else look up coordinator in registry, request it to publish
-        Registry registry = LocateRegistry.getRegistry(coordHost, coordPort);
-        ServerToServerInterface coordServer = (ServerToServerInterface)
+
+        try{
+            // else look up coordinator in registry, request it to publish
+            Registry registry = LocateRegistry.getRegistry(coordHost, coordPort);
+            ServerToServerInterface coordServer = (ServerToServerInterface)
             registry.lookup("BulletinBoardServer_" + coordNum);
-        return coordServer.CoordinatorRead();
+            return coordServer.CoordinatorRead();
+        } catch (Exception e){
+            return false;
+        }
     }
+    */
 
     /**
      * @param articleID ID of article requested in full
-     */
-    public String Choose(int articleID) throws RemoteException {
+     
+    public boolean Choose(int articleID) throws RemoteException {
         if (coordNum == serverNum) {
-            return cStrat.ServerChoose(articleID, contentTree);
+            //return cStrat.ServerChoose(articleID, contentTree);
+            return true;
         }
-        // else look up coordinator in registry, request it to publish
-        Registry registry = LocateRegistry.getRegistry(coordHost, coordPort);
-        ServerToServerInterface coordServer = (ServerToServerInterface)
+
+        try{
+            // else look up coordinator in registry, request it to publish
+            Registry registry = LocateRegistry.getRegistry(coordHost, coordPort);
+            ServerToServerInterface coordServer = (ServerToServerInterface)
             registry.lookup("BulletinBoardServer_" + coordNum);
-        return coordServer.CoordinatorChoose(articleID);
+            return coordServer.CoordinatorChoose(articleID);
+        } catch (Exception e){
+            return false;
+        }
     }
+    */
 
     /**
      * from ServerToServerInterface
@@ -162,11 +204,11 @@ implements BulletinBoardServerInterface, ServerToServerInterface {
      * @param replyTo ID of article to reply to
      * this server should be the coordinator
      * calls ServerPublish() using strategy object
-     */
+     
     public boolean CoordinatorPost(String article, int replyTo)
             throws RemoteException {
         return cStrat.ServerPublish(article, serverPort, nextID++);
-    }
+    } */
 
     /**
      * from ServerToServerInterface
@@ -175,35 +217,50 @@ implements BulletinBoardServerInterface, ServerToServerInterface {
      * @param article string consisting of article
      * @param replyTo specify article to reply to 
      * if 0, it replies to root, which is just a new post
-     */
+     
     public boolean UpdateTree(int newID, String article, int replyTo)
                                 throws RemoteException {
-        return contentTree.AddNode(newID, article, replyTo);
-    }
+        //return contentTree.AddNode(newID, article, replyTo);
+        return true;
+    } */
 
     /**
      * should only be called on coordinator be non-coordinator
      * coordinator uses strategy to return article preview based
      * on chosen consistency
-     */
+     
     public String CoordinatorRead() throws RemoteException {
         // TODO: just call the consistency strategy onject, maybe control for some errors
         return "";
-    }
+    } 
+
+    public boolean CoordinatorRead() throws RemoteException {
+        // TODO: just call the consistency strategy onject, maybe control for some errors
+        return false;
+    } */
 
     /**
      * from ServerToServerInterface
      * should only be called on coordinator server object
      * @param articleID ID if article to return in full
      * returns message if article isn't found
-     */
-    public String CoordinatorChoose(articleID) throws RemoteException {
+     
+    public String CoordinatorChoose(int articleID) throws RemoteException {
         cStrat.ServerChoose(articleID);
+    } 
+
+    public boolean CoordinatorPost(String article) throws RemoteException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'CoordinatorPost'");
     }
 
+    public boolean CoordinatorChoose(int articleID) throws RemoteException {
+        //cStrat.ServerChoose(articleID);
+        return false;
+    } */
 
     private static boolean CheckValidPort(int port){
-        Set<int> ports = new HashSet<>();
+        Set<Integer> ports = new HashSet<>();
         ports.add(2000);
         ports.add(2001);
         ports.add(2002);
@@ -215,8 +272,8 @@ implements BulletinBoardServerInterface, ServerToServerInterface {
 
     public static void main(String[] args){
         // If no argument is specified, then print error message and exit
-        if (args.length != 1){
-            System.out.println("\n[SERVER]: Usage: java BulletinBoardServer <port>");
+        if (args.length != 2){
+            System.out.println("\n[SERVER]: Usage: java BulletinBoardServer <port> <consistency>");
             System.out.println("[SERVER]: Exiting...");
             System.exit(0);
         }
@@ -245,7 +302,7 @@ implements BulletinBoardServerInterface, ServerToServerInterface {
             registry.rebind("BulletinBoardServer_" + serverNum, server);
             System.out.printf("\n[SERVER]: Bulletin Board Server %d is ready at port %d. \n", serverNum, port);
         } catch(Exception e) {
-            System.out.println("[SERVER]: Error occurred while launching server. It's possible that the port specified is currently in use.");
+            System.out.println("\n[SERVER]: Error occurred while launching server. It's possible that the port specified is currently in use.");
             System.out.println("[SERVER]: Exiting...");
             System.exit(0);
         }
