6825761a700e6ee50a2289b2afcd46c928d09174
cloutburst <jashwinacharyapersonal@gmail.com>
2023-03-15 18:12:48 -0500

    Implemented skeleton code for Read-Your-Write strategy

--- Project_2/src/ReadYourWritesStrategy.java
+++ Project_2/src/ReadYourWritesStrategy.java
@@ -10,15 +10,53 @@ import java.net.InetAddress;
 public class ReadYourWritesStrategy implements ConsistencyStrategy {
     public boolean ServerPublish(String article, int replyTo,
                         BulletinBoardServer selfServer) {
-        return false;
+        // TODO: Check if at least 2 servers are online
+    
+        // Only coordinator server can make updates locally
+        try{
+            // Contact coordinator
+            Registry registry = LocateRegistry.getRegistry(selfServer.GetCoordHost(), selfServer.GetCoordPort());
+
+            // Check if the object calling ServerPublish is itself the coordinator
+            BulletinBoardServer server = selfServer;
+            if (selfServer.GetServerPort() != selfServer.GetCoordPort()){
+                server = (BulletinBoardServer) registry.lookup("BulletinBoardServer_" + selfServer.GetCoordNum());
+            }
+
+            // Update data only on the central server
+            int nextID = selfServer.GetCurrID();
+            if (!selfServer.GetTree().AddNode(nextID, article, replyTo)) {
+                return false;  // local update failed, do not update others
+            }
+            
+            // TODO: Once primary server has made its updates, lazily update the read servers (will need a thread here to do that)
+            return true;
+        } catch (Exception e){
+            return false;
+        }
+
+        
     }
 
+    /**
+     * @param selfServer server object that called this method
+     * sequential consistency, just read from local
+     */
     public String ServerRead(BulletinBoardServer selfServer) {
-        return "";
+        return selfServer.GetTree().ReadTree();
     }
 
+    /**
+     * @param articleID article requested by client
+     * @param contentTree article tree from server object that called this
+     * sequential consistency, just read from local
+     */
     public String ServerChoose(int articleID, ReferencedTree contentTree) {
-        return "";
+        String result = contentTree.GetAtIndex(articleID);
+        if (result == null) {
+            return "Article not found for ID: " + articleID;
+        }
+        return result;
     }
 
 }
